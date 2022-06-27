226d227b4e6230757cb27752f7bc297764f88e25
Vajreshwari Chougule <v.chougule@zensar.com>
2022-06-27 18:26:55 +0530

    CodeBase_5

---
+++ CodeBase_5/CodeBase_5/CodeBase_5/Data/CodeBase_5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeBase_5.Models;
+
+namespace CodeBase_5.Data
+{
+    public class CodeBase_5Context : DbContext
+    {
+        public CodeBase_5Context (DbContextOptions<CodeBase_5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeBase_5.Models.Student> Student { get; set; }
+    }
+}
