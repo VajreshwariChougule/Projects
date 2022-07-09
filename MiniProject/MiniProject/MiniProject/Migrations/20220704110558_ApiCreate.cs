099df0bf92a6582e9e1123b0ac68198e706db4d0
Vajreshwari Chougule <v.chougule@zensar.com>
2022-07-09 13:02:40 +0530

    FinalProject

---
+++ MiniProject/MiniProject/MiniProject/Migrations/20220704110558_ApiCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniProject.Migrations
+{
+    public partial class ApiCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManufactureDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ MiniProject/MiniProject/MiniProject/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MiniProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double? Price { get; set; }
+
+        public string Brand { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0 : yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string ManufactureDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        public string ExpirationDate { get; set; }
+    }
+}
